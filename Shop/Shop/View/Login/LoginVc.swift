0eb5e712ff877e4d41a59939c0a67007a43ababf
salma <salmaelawady123@gmail.com>
2022-05-29 20:46:19 +0200

    login design

--- Shop/Shop/View/Login/LoginVc.swift
+++ Shop/Shop/View/Login/LoginVc.swift
@@ -2,20 +2,37 @@
 //  LoginVc.swift
 //  Shop
 //
-//  Created by Salma on 5/25/22.
+//  Created by Salma on 29/05/2022.
 //
 
 import UIKit
 
 class LoginVc: UIViewController {
 
+    @IBOutlet weak var signUpBtn: UIButton!
+    @IBOutlet weak var skipBtn: UIButton!
+    @IBOutlet weak var signInBtn: UIButton!
+    @IBOutlet weak var passTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var loginImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        signInBtn.backgroundColor = .blue
+        signInBtn.layer.cornerRadius=20
+        skipBtn.backgroundColor = .blue
+        skipBtn.layer.cornerRadius=20
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func SignInBtn(_ sender: Any) {
+    }
+    
+    @IBAction func skipBtn(_ sender: Any) {
+    }
+    
+    @IBAction func sinUpBtn(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
