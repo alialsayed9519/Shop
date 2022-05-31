1ee4877b7b5707d1f1d195b3cb117de0b6e9f3b2
salma <salmaelawady123@gmail.com>
2022-05-31 15:24:01 +0200

    collection model and structs and urls and product struct

--- Shop/Shop/Modles/NetworkService/NetworkService.swift
+++ Shop/Shop/Modles/NetworkService/NetworkService.swift
@@ -7,10 +7,43 @@
 
 import Foundation
 import Alamofire
-
-class NetworkService {
-    
-    static func getAllCategories(completion: @escaping ([ SmartCollection]?, Error?) -> ()) {
-        
+//class NetworkService {
+//
+//    static func getAllCategories(completion: @escaping ([SmartCollection]?, Error?) -> ()) {
+//
+//
+//    }
+//
+func fetchCustomCatagegories(completion:@escaping ([CustomCollections]?,Error?)->()){
+        AF.request(URLs.customCollections()).validate().responseDecodable(of:CustomCollection.self){
+            (response) in
+            switch response.result{
+        case .success(_):
+            guard let data=response.value
+            else{return}
+                completion(data.custom_collections,nil)
+            case .failure(let error):
+                completion(nil,error)
+            }
+            
+        }
     }
-}
+     func getProducts(collectionID:String,completion:@escaping([Product]?,Error?)->()){
+        AF.request(URLs.products(collectionId: collectionID)).validate().responseDecodable(of:AllProducts.self){
+            (response) in
+            switch response.result{
+            case .success(_):
+                guard let data=response.value
+                else {
+                    return
+                }
+                completion(data.products,nil)
+            case .failure(let error) :
+                completion(nil,error)
+            
+                
+            }
+        }
+    }
+
+
