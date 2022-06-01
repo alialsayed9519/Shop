b65b8dd0ca5b3918b3ed6a555a524677b2eb3ffe
ALi Alsayed <ali.alsayed9519@gmail.com>
2022-06-01 16:41:46 +0200

    add navigation to cart and Favorite

--- Shop/Shop/Modles/PONSOs/Collections.swift
+++ Shop/Shop/Modles/PONSOs/Collections.swift
@@ -6,17 +6,30 @@
 //
 
 import Foundation
-struct CustomCollections:Codable{
-    var custom_collections:[CustomCollection]
+struct CustomCollections: Codable{
+    var custom_collections: [CustomCollection]?
 }
 
-struct CustomCollection:Codable{
-var id:Int?
-var title:String?
-var image:[collectionImage]?
+struct CustomCollection: Codable{
+    var id: Int?
+    var handle: String?
+    var title:String?
+    var updated_at: String?
+    var body_html: String?
+    var published_at: String?
+    var sort_order: String?
+    var template_suffix: String?
+    var published_scope: String?
+    var admin_graphql_api_id: String?
+    var image: collectionImage?
+    
+    
+    
 }
 
-struct collectionImage:Codable{
+struct collectionImage: Codable{
+    var created_at: String?
+    var alt: String?
     var width:Double?
     var height:Double?
     var src:String?
