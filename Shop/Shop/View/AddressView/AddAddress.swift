3375955ecb2a395798e81700587d39055ebaf000
yasmeenhosny98 <yasmeenhosny98.1@gmail.com>
2022-06-07 18:12:59 +0200

    create the add address screen ui

---
+++ Shop/Shop/View/AddressView/AddAddress.swift
@@ -0,0 +1,27 @@
+//
+//  AddAddress.swift
+//  Shop
+//
+//  Created by yasmeen hosny on 6/7/22.
+//
+
+import UIKit
+
+class AddAddress: UIViewController {
+    
+    @IBOutlet weak var TFCountry: UITextField!
+    @IBOutlet weak var TFCity: UITextField!
+    @IBOutlet weak var TFAddress: UITextField!
+    @IBOutlet weak var TFPhone: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addAddress(_ sender: Any) {
+        
+    }
+    
+}
