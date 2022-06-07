8b251900a9dd3018bd8642f4fa94e31e1c864b70
yasmeenhosny98 <yasmeenhosny98.1@gmail.com>
2022-06-07 19:08:53 +0200

    add address cell UI

---
+++ Shop/Shop/View/Cells/Address/AddressCell.swift
@@ -0,0 +1,29 @@
+//
+//  AddressCell.swift
+//  Shop
+//
+//  Created by yasmeen hosny on 6/7/22.
+//
+
+import UIKit
+
+class AddressCell: UITableViewCell {
+
+    @IBOutlet weak var country: UILabel!
+    @IBOutlet weak var cityAddress: UILabel!
+    @IBOutlet weak var phone: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func editAddress(_ sender: Any) {
+        
+    }
+}
