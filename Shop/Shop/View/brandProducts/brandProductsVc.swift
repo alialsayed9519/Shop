8c590ebcaaf41f2f3130046f3eaa5f2be1878b22
salma <salmaelawady123@gmail.com>
2022-06-14 19:42:15 +0200

    updateInBrandProduct

--- Shop/Shop/View/brandProducts/brandProductsVc.swift
+++ Shop/Shop/View/brandProducts/brandProductsVc.swift
@@ -10,32 +10,24 @@ import SDWebImage
 class brandProductsVc: UIViewController {
     private let shopViewModel = ShopingViewModel()
    
-    @IBOutlet weak var brandImg: UIImageView!
-    let productCell="productCell"
-    var smartCol:SmartCollection?
-    var product:Product!
     @IBOutlet weak var productCollectionView: UICollectionView!
- 
+    @IBOutlet weak var brandImg: UIImageView!
+    
+    let productCell="productCell"
+    var smartCol:SmartCollection!
+    var product:Product!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let brandProductCell=UINib(nibName:productCell , bundle: nil)
                 productCollectionView.register(brandProductCell, forCellWithReuseIdentifier:productCell)
-        
-    //  brandImg.sd_setImage(with:URL(string:smartCol?.image
-//.src?),placeholderImage:UIImage(named:"adidas.png"))
-//        brandImg.sd_setImage(with: URL(string: smartCol?.image), placeholderImage: UIImage(named: "adidas,png"))
-//        brandImg.sd_setImage(with: URL(string: smartCol?.image.src?), placeholderImage: UIImage(named: "adidas.png"))
-      //  shopViewModel.filterBrandsByNmae(brandName:smartCol?.title?)
-    //shopViewModel.filterBrandsByNmae(brandName:smartCol?.title!)
-       // let imageView = SDAnimatedImageView()
-        let animatedImage = SDAnimatedImage(named: (smartCol?.image.src)!)
-        brandImg.image = animatedImage
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+
+        brandImg.sd_setImage(with:URL(string:smartCol.image.src), placeholderImage: UIImage(named: "adidas.png"))
+        shopViewModel.filterBrandsByNmae(brandName:smartCol.title)
         // Do any additional setup after loading the view.
     }
 
-    func display(){
-        
-    }
     /*
     // MARK: - Navigation
 
@@ -59,8 +51,8 @@ extension brandProductsVc:UICollectionViewDelegate,UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let proCell=collectionView.dequeueReusableCell(withReuseIdentifier: productCell, for: indexPath) as! productCell
-        proCell.productName.text=product.title
-        proCell.productImg.sd_setImage(with: URL(string: product.images[0].src), placeholderImage: UIImage(named: "adidas"))
+        proCell.productName.text=product?.title
+        proCell.productImg.sd_setImage(with: URL(string: (product?.images[0].src)!),placeholderImage: UIImage(named: "adidas"))
         return proCell
     }
 }
