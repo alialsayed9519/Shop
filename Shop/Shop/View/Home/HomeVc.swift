1947cf1afbe57b0ffc566062b59c2af0a5150863
salma <salmaelawady123@gmail.com>
2022-06-19 19:19:22 +0200

    payPal

--- Shop/Shop/View/Home/HomeVc.swift
+++ Shop/Shop/View/Home/HomeVc.swift
@@ -94,9 +94,9 @@ extension HomeVc: UICollectionViewDataSource, UICollectionViewDelegate,UICollect
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let brand = brands[indexPath.row]
-        let proVc=brandProductsVc()
-        proVc.smartCol=brand
-        self.navigationController?.pushViewController(proVc, animated: true)
+    //    let proVc=brandProductsVc()
+       // proVc.smartCol=brand
+      //  self.navigationController?.pushViewController(proVc, animated: true)
     }
 }
 
--- Shop/Shop/payment/PaymentVc.swift
+++ Shop/Shop/payment/PaymentVc.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
-import Braintree
+//import Braintree
+import BraintreeDropIn
 class PaymentVc: UIViewController {
-    var braintreeClient: BTAPIClient!
+ // var braintreeClient: BTAPIClient!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,32 +20,68 @@ class PaymentVc: UIViewController {
 
 
     func payMent(){
-        braintreeClient = BTAPIClient(authorization: "sandbox_v26b7763_zchjhvj48cst95wd")!
-        let payPalDriver = BTPayPalDriver(apiClient: braintreeClient)
+//        braintreeClient = BTAPIClient(authorization: "sandbox_v26b7763_zchjhvj48cst95wd")!
+//        let payPalDriver = BTPayPalDriver(apiClient: braintreeClient)
+//
+//                // Specify the transaction amount here. "2.32" is used in this example.
+//                let request = BTPayPalCheckoutRequest(amount: "2.32")
+//                request.currencyCode = "USD" // Optional; see BTPayPalCheckoutRequest.h for more options
+//
+//                payPalDriver.tokenizePayPalAccount(with: request) { (tokenizedPayPalAccount, error) in
+//                    if let tokenizedPayPalAccount = tokenizedPayPalAccount {
+//                        print("Got a nonce: \(tokenizedPayPalAccount.nonce)")
+//
+//                        // Access additional information
+//                        let email = tokenizedPayPalAccount.email
+//                        let firstName = tokenizedPayPalAccount.firstName
+//                        let lastName = tokenizedPayPalAccount.lastName
+//                        let phone = tokenizedPayPalAccount.phone
+//
+//                        // See BTPostalAddress.h for details
+//                        let billingAddress = tokenizedPayPalAccount.billingAddress
+//                        let shippingAddress = tokenizedPayPalAccount.shippingAddress
+//                    } else if let error = error {
+//                        // Handle error here...
+//                    } else {
+//                        // Buyer canceled payment approval
+//                    }
+//                }
+        print("salmaaaaaa")
+        func fetchClientToken() {
+            // TODO: Switch this URL to your own authenticated API
+            let clientTokenURL = NSURL(string: "sandbox_v26b7763_zchjhvj48cst95wd")!
+            let clientTokenRequest = NSMutableURLRequest(url: clientTokenURL as URL)
+            clientTokenRequest.setValue("text/plain", forHTTPHeaderField: "Accept")
 
-                // Specify the transaction amount here. "2.32" is used in this example.
-                let request = BTPayPalCheckoutRequest(amount: "2.32")
-                request.currencyCode = "USD" // Optional; see BTPayPalCheckoutRequest.h for more options
+            URLSession.shared.dataTask(with: clientTokenRequest as URLRequest) { (data, response, error) -> Void in
+                // TODO: Handle errors
+                let clientToken = String(data: data!, encoding: String.Encoding.utf8)
 
-                payPalDriver.tokenizePayPalAccount(with: request) { (tokenizedPayPalAccount, error) in
-                    if let tokenizedPayPalAccount = tokenizedPayPalAccount {
-                        print("Got a nonce: \(tokenizedPayPalAccount.nonce)")
-
-                        // Access additional information
-                        let email = tokenizedPayPalAccount.email
-                        let firstName = tokenizedPayPalAccount.firstName
-                        let lastName = tokenizedPayPalAccount.lastName
-                        let phone = tokenizedPayPalAccount.phone
-
-                        // See BTPostalAddress.h for details
-                        let billingAddress = tokenizedPayPalAccount.billingAddress
-                        let shippingAddress = tokenizedPayPalAccount.shippingAddress
-                    } else if let error = error {
-                        // Handle error here...
-                    } else {
-                        // Buyer canceled payment approval
-                    }
-                }
+                // As an example, you may wish to present Drop-in at this point.
+                // Continue to the next section to learn more...
+                }.resume()
+        }
+        showDropIn(clientTokenOrTokenizationKey: "sandbox_v26b7763_zchjhvj48cst95wd")
+    }
+    func showDropIn(clientTokenOrTokenizationKey: String) {
+        let request =  BTDropInRequest()
+        let dropIn = BTDropInController(authorization: clientTokenOrTokenizationKey, request: request)
+        { (controller, result, error) in
+            if (error != nil) {
+                print("ERROR")
+            } else if (result?.isCanceled == true) {
+                print("CANCELED")
+            } else if let result = result {
+                print("sucessfully paid")
+                // Use the BTDropInResult properties to update your UI
+                // result.paymentMethodType
+                // result.paymentMethod
+                // result.paymentIcon
+                // result.paymentDescription
+            }
+            controller.dismiss(animated: true, completion: nil)
+        }
+        self.present(dropIn!, animated: true, completion: nil)
     }
     @IBAction func pay(_ sender: Any) {
         payMent()
