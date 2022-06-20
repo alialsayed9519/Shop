dce7dfbf1009e7975c601080205e92398050269b
salma <salmaelawady123@gmail.com>
2022-06-20 12:48:20 +0200

    profile

---
+++ Shop/Shop/View/Profile/GuestProfileVc.swift
@@ -0,0 +1,37 @@
+//
+//  GuestProfileVc.swift
+//  Shop
+//
+//  Created by Salma on 20/06/2022.
+//
+
+import UIKit
+
+class GuestProfileVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func goToLogin(_ sender: Any) {
+        self.navigationController?.pushViewController(loginvc(), animated: true)
+    }
+    
+    
+    @IBAction func goToRegister(_ sender: Any) {
+        self.navigationController?.pushViewController(registerVc(), animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Shop/Shop/payment/PaymentVc.swift
+++ Shop/Shop/payment/PaymentVc.swift
@@ -7,84 +7,87 @@
 
 import UIKit
 //import Braintree
-import BraintreeDropIn
+//import BraintreeDropIn
 class PaymentVc: UIViewController {
 
     @IBOutlet weak var discountView: UIView!
     @IBOutlet weak var paymentView: UIView!
-    @IBOutlet weak var detailsView: UIView!
     @IBOutlet weak var cashButton: UIButton!
     @IBOutlet weak var onlineButton: UIButton!
-    @IBOutlet weak var continueButton: UIButton!
-    @IBOutlet weak var TFcopun: UITextField!
-    @IBOutlet weak var applyButton: UIButton!
-    @IBOutlet weak var LPrice: UILabel!
-    @IBOutlet weak var LDiscount: UILabel!
-    @IBOutlet weak var LShipping: UILabel!
-    @IBOutlet weak var LTotale: UILabel!
-   
-  var order: Order?
-    var price: Int?
-    var total: Int?
-    var currency: String?
     
-    private let viewModel = OrderViewModel()
-
+    var orderAddress: Address?
+    var orderItems: [LineItems]?
+    var customer: Customer?
+    var discountCode: Price_Rule?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       
         // Do any additional setup after loading the view.
-      
-        self.discountView.layer.cornerRadius = 20
-        self.paymentView.layer.cornerRadius = 20
-        self.detailsView.layer.cornerRadius = 20
-        self.continueButton.layer.cornerRadius = 20
-        self.applyButton.layer.cornerRadius = 20
-        
-        price = Int((order?.current_total_price) ?? "0") ?? 0
-        total = price! + 15
-        currency = userDefault().getCurrency()
-        
-        self.LPrice.text = "\(String(describing: price)).00 \(String(describing: currency))"
-        self.LDiscount.text = "0.00 \(String(describing: currency))"
-        self.LShipping.text = "15.00 \(String(describing: currency))"
-        self.LTotale.text = "\(String(describing: total)).00 \(String(describing: currency))"
     }
 
 
 
     func payMent(){
-        print("salmaaaaaa")
-        func fetchClientToken() {
-            let clientTokenURL = NSURL(string: "sandbox_v26b7763_zchjhvj48cst95wd")!
-            let clientTokenRequest = NSMutableURLRequest(url: clientTokenURL as URL)
-            clientTokenRequest.setValue("text/plain", forHTTPHeaderField: "Accept")
-
-            URLSession.shared.dataTask(with: clientTokenRequest as URLRequest) { (data, response, error) -> Void in
-                // TODO: Handle errors
-                let clientToken = String(data: data!, encoding: String.Encoding.utf8)
-
-                // As an example, you may wish to present Drop-in at this point.
-                // Continue to the next section to learn more...
-                }.resume()
-        }
-        showDropIn(clientTokenOrTokenizationKey: "sandbox_v26b7763_zchjhvj48cst95wd")
-    }
-    func showDropIn(clientTokenOrTokenizationKey: String) {
-        let request =  BTDropInRequest()
-        let dropIn = BTDropInController(authorization: clientTokenOrTokenizationKey, request: request)
-        { (controller, result, error) in
-            if (error != nil) {
-                print("ERROR")
-            } else if (result?.isCanceled == true) {
-                print("CANCELED")
-            } else if let result = result {
-                print("sucessfully paid")
-                // Use the BTDropInResult properties to update your UI
-                // result.paymentMethodType
-                // result.paymentMethod
-                // result.paymentIcon
-                // result.paymentDescription
+//        braintreeClient = BTAPIClient(authorization: "sandbox_v26b7763_zchjhvj48cst95wd")!
+//        let payPalDriver = BTPayPalDriver(apiClient: braintreeClient)
+//
+//                // Specify the transaction amount here. "2.32" is used in this example.
+//                let request = BTPayPalCheckoutRequest(amount: "2.32")
+//                request.currencyCode = "USD" // Optional; see BTPayPalCheckoutRequest.h for more options
+//
+//                payPalDriver.tokenizePayPalAccount(with: request) { (tokenizedPayPalAccount, error) in
+//                    if let tokenizedPayPalAccount = tokenizedPayPalAccount {
+//                        print("Got a nonce: \(tokenizedPayPalAccount.nonce)")
+//
+//                        // Access additional information
+//                        let email = tokenizedPayPalAccount.email
+//                        let firstName = tokenizedPayPalAccount.firstName
+//                        let lastName = tokenizedPayPalAccount.lastName
+//                        let phone = tokenizedPayPalAccount.phone
+//
+//                        // See BTPostalAddress.h for details
+//                        let billingAddress = tokenizedPayPalAccount.billingAddress
+//                        let shippingAddress = tokenizedPayPalAccount.shippingAddress
+//                    } else if let error = error {
+//                        // Handle error here...
+//                    } else {
+//                        // Buyer canceled payment approval
+//                    }
+//                }
+//        print("salmaaaaaa")
+//        func fetchClientToken() {
+//            // TODO: Switch this URL to your own authenticated API
+//            let clientTokenURL = NSURL(string: "sandbox_v26b7763_zchjhvj48cst95wd")!
+//            let clientTokenRequest = NSMutableURLRequest(url: clientTokenURL as URL)
+//            clientTokenRequest.setValue("text/plain", forHTTPHeaderField: "Accept")
+//
+//            URLSession.shared.dataTask(with: clientTokenRequest as URLRequest) { (data, response, error) -> Void in
+//                // TODO: Handle errors
+//                let clientToken = String(data: data!, encoding: String.Encoding.utf8)
+//
+//                // As an example, you may wish to present Drop-in at this point.
+//                // Continue to the next section to learn more...
+//                }.resume()
+//        }
+//        showDropIn(clientTokenOrTokenizationKey: "sandbox_v26b7763_zchjhvj48cst95wd")
+//    }
+//    func showDropIn(clientTokenOrTokenizationKey: String) {
+//        let request =  BTDropInRequest()
+//        let dropIn = BTDropInController(authorization: clientTokenOrTokenizationKey, request: request)
+//        { (controller, result, error) in
+//            if (error != nil) {
+//                print("ERROR")
+//            } else if (result?.isCanceled == true) {
+//                print("CANCELED")
+//            } else if let result = result {
+//                print("sucessfully paid")
+//                // Use the BTDropInResult properties to update your UI
+//                // result.paymentMethodType
+//                // result.paymentMethod
+//                // result.paymentIcon
+//                // result.paymentDescription
             }
             controller.dismiss(animated: true, completion: nil)
         }
@@ -92,37 +95,16 @@ class PaymentVc: UIViewController {
     }
     @IBAction func pay(_ sender: Any) {
         payMent()
+        self.discountView.layer.cornerRadius = 20
+        self.paymentView.layer.cornerRadius = 20
+        
     }
+
+   
     @IBAction func cashPayment(_ sender: Any) {
-        order?.gateway = "Cash On Delivery"
     }
     
     @IBAction func onlinePayment(_ sender: Any) {
-        order?.gateway = "Paypal"
     }
-    @IBAction func continuePayment(_ sender: Any) {
-        order?.currency = self.currency
-        order?.email = userDefault().getEmail()
-        switch order?.gateway {
-        case "Cash On Delivery":
-            viewModel.postOrder(order: order!)
-        //    self.navigationController?.pushViewController(, animated: <#T##Bool#>)
-        default:
-            print("Ay 7aga")
-        }
-    }
-    
-    @IBAction func applydiscountCode(_ sender: Any) {
-        let discount = viewModel.checkDescountCode(copun: TFcopun.text ?? "")
-        utils.showAlert(message: discount.1, title: "Discount Code", view: self)
-        total = 15 + price! - (price! * (discount.0 / 100))
-        
-        //MARK: - Update Order
-        order?.discount = "\(discount.0)"
-        order?.current_total_price = "total"
-        
-        //MARK: - Update labels
-        self.LDiscount.text = "\(discount.0).00 \(String(describing: currency))"
-        self.LTotale.text = "\(String(describing: total)).00 \(String(describing: currency))"
-    }    
 }
+
