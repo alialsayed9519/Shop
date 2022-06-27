ea772823e0c27d3a98f9b231fafec2b5ff57460f
salma <salmaelawady123@gmail.com>
2022-06-27 14:25:39 +0200

    onBoardView

--- Shop/Shop/View/OnBoard/OnBoardVc.swift
+++ Shop/Shop/View/OnBoard/OnBoardVc.swift
@@ -9,42 +9,37 @@ import UIKit
 
 class OnBoardVc: UIViewController , UIScrollViewDelegate{
 
-    @IBOutlet weak var getstarted: UIButton!
-    
-    @IBOutlet weak var signIn: UIButton!
-
-    @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var scrollView: UIScrollView!
-    
-    var scrollWidth: CGFloat! = 0.0
+        @IBOutlet weak var pagecontrol: UIPageControl!
+        @IBOutlet weak var getStartedBtn: UIButton!
+        
+        // MARK: - Properties
+        var scrollWidth: CGFloat! = 0.0
         var scrollHeight: CGFloat! = 0.0
+        var titles = ["Explore many products","Choose and checkout","Get it delivered"]
+        var descs = ["Best Prices on your favourite brands","search latest products for the desired","Just Choose it!"]
+        var imgs = ["shop","user","shoes"]
+    
+        
+        // MARK: - Life cycle
+            override func viewDidLayoutSubviews() {
+                scrollWidth = scrollView.frame.size.width
+                scrollHeight = scrollView.frame.size.height
+            }
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
 
-        //data for the slides
-        var titles = ["FAST DELIVERY","EXCITING OFFERS","SECURE PAYMENT"]
-        var descs = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.","Lorem ipsum dolor sit amet, consectetur adipiscing elit.","Lorem ipsum dolor sit amet, consectetur adipiscing elit."]
-        var imgs = ["shop","user","bag"]
-
-        //get dynamic width and height of scrollview and save it
-        override func viewDidLayoutSubviews() {
-            scrollWidth = scrollView.frame.size.width
-            scrollHeight = scrollView.frame.size.height
-        }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        getstarted.layer.cornerRadius=20
-        
-        
-        
-        self.view.layoutIfNeeded()
-        // Do any additional setup after loading the view.
-        self.scrollView.delegate = self
+            override func viewDidLoad() {
+                super.viewDidLoad()
+                self.view.layoutIfNeeded()
+                UserDefaults.standard.set(true, forKey: "hasSeenOnBoard")
+                self.scrollView.delegate = self
                 scrollView.isPagingEnabled = true
                 scrollView.showsHorizontalScrollIndicator = false
                 scrollView.showsVerticalScrollIndicator = false
 
-                //crete the slides and add them
                 var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
-
                 for index in 0..<titles.count {
                     frame.origin.x = scrollWidth * CGFloat(index)
                     frame.size = CGSize(width: scrollWidth, height: scrollHeight)
@@ -61,51 +56,50 @@ class OnBoardVc: UIViewController , UIScrollViewDelegate{
                     txt1.textAlignment = .center
                     txt1.font = UIFont.boldSystemFont(ofSize: 20.0)
                     txt1.text = titles[index]
-
-                    let txt2 = UILabel.init(frame: CGRect(x:32,y:txt1.frame.maxY+10,width:scrollWidth-90,height:50))
+                   
+                    
+                    let txt2 = UILabel.init(frame: CGRect(x:32,y:txt1.frame.maxY+10,width:scrollWidth-64,height:50))
                     txt2.textAlignment = .center
                     txt2.numberOfLines = 3
                     txt2.font = UIFont.systemFont(ofSize: 18.0)
                     txt2.text = descs[index]
-
+                    
                     slide.addSubview(imageView)
                     slide.addSubview(txt1)
                     slide.addSubview(txt2)
                     scrollView.addSubview(slide)
                 }
-        //set width of scrollview to accomodate all the slides
+
+                //set width of scrollview to accomodate all the slides
                 scrollView.contentSize = CGSize(width: scrollWidth * CGFloat(titles.count), height: scrollHeight)
 
                 //disable vertical scroll/bounce
                 self.scrollView.contentSize.height = 1.0
 
                 //initial state
-                pageControl.numberOfPages = titles.count
-                pageControl.currentPage = 0
+                pagecontrol.numberOfPages = titles.count
+                pagecontrol.currentPage = 0
 
-    }
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-            setIndiactorForCurrentPage()
+            }
+        // MARK:  handlers
+            //indicator
+            @IBAction func pageChanged(_ sender: Any) {
+                scrollView!.scrollRectToVisible(CGRect(x: scrollWidth * CGFloat ((pagecontrol?.currentPage)!), y: 0, width: scrollWidth, height: scrollHeight), animated: true)
+            }
+
+            func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+                setIndiactorForCurrentPage()
+            }
+
+            func setIndiactorForCurrentPage()  {
+                let page = (scrollView?.contentOffset.x)!/scrollWidth
+                pagecontrol?.currentPage = Int(page)
+            }
+       // MARK: - IBActions
+        @IBAction func getStartedAction(_ sender: Any) {
+            
+            let home = MyTabBar(nibName: "MyTabBar", bundle: nil)
+            self.navigationController?.pushViewController(home, animated: true)
         }
-
-        func setIndiactorForCurrentPage()  {
-            let page = (scrollView?.contentOffset.x)!/scrollWidth
-            pageControl?.currentPage = Int(page)
-        }
-
-    @IBAction func pageChanged(_ sender: Any) {
-        scrollView!.scrollRectToVisible(CGRect(x: scrollWidth * CGFloat ((pageControl?.currentPage)!), y: 0, width: scrollWidth, height: scrollHeight), animated: true)
-    }
-    
-    
-    @IBAction func gotoHome(_ sender: Any) {
-        let home = MyTabBar(nibName: "MyTabBar", bundle: nil)
-        self.navigationController?.pushViewController(home, animated: true)
         
-    }
-    
-    @IBAction func gotoLogin(_ sender: Any) {
-        self.navigationController?.pushViewController(loginvc(), animated: true)
-    }
-    
 }
